b8fae07ba93b34269a89c4887e7cec932fada23b
Christy <jchr@a45e60bb5c69.ant.amazon.com>
2018-03-03 18:33:54 +0000

    Added SpaineyCup as comp, plus leaderboardDetailRound

---
+++ NativeApp/leaderboardDetailRound.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Image, View, StyleSheet, Text, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
+import styles from './Styles'
+
+export default class LeaderboardDetailRound extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View>
+                <Text>foo</Text>
+            </View>
+        );
+    }
+
+}
+;
--- NativeApp/leaderboardWidget.js
+++ NativeApp/leaderboardWidget.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from 'react-nati
 import styles from './Styles'
 import LeaderboardComps from './leaderboardComps';
 import LeaderboardDetail from './leaderboardDetail';
+import LeaderboardDetailRound from './leaderboardDetailRound';
 
 export default class LeaderboardWidget extends Component {
 	constructor(props){
@@ -20,17 +21,11 @@ export default class LeaderboardWidget extends Component {
             
             compList.push(
                 <TouchableOpacity onPress={() => {this.props.onCompIdChange(compId);this.refs._compDetailScrollView.scrollTo({x:0, y:0, animated: false})}} key={i} >
-                <LeaderboardComps 
-                    compHeroImage={comps[i].imgUrlHero}
-                    compName={comps[i].name}
-                    compCourse={comps[i].course}
-                    compName={comps[defaultCompId].name}
-                    compId={comps[defaultCompId].id}
-                    compCourse={comps[defaultCompId].course}
-                    scoreData={this.props.scoreData} 
-                    playerData={this.props.playerData} 
-                    compData={this.props.compData} 
-                />
+                    <LeaderboardComps 
+                        compHeroImage={comps[i].imgUrlHero}
+                        compName={comps[i].name}
+                        compCourse={comps[i].course}
+                    />
                 </TouchableOpacity>
             );
         }
@@ -44,23 +39,32 @@ export default class LeaderboardWidget extends Component {
                          playerData={this.props.playerData} 
                     />;
 
+        var defaultCompDetailRound = 
+                    <LeaderboardDetailRound
+                         compName={comps[defaultCompId].name}
+                         compId={comps[defaultCompId].id}
+                         compCourse={comps[defaultCompId].course}
+                         scoreData={this.props.scoreData} 
+                         playerData={this.props.playerData} 
+                    />;
+
 		return (
 			<View style={{flex: 4}}>
-            <View style={{marginBottom: 2, marginTop: 2}}>
-                <ScrollView horizontal={true} ref='_compImageListView' showsHorizontalScrollIndicator={false}>
-                    {compList}
-                </ScrollView>
-            </View>
-            <View>
-                <ScrollView ref='_compDetailScrollView'>
-                    {defaultComp}
-                </ScrollView>
-            </View>
-            <View style={{flex: 1}}>
-                <ScrollView horizontal={true} ref='_compPlayerDetailScrollView' showsHorizontalScrollIndicator={false}>
-                   <Text>player round details</Text> 
-                </ScrollView>
-			</View>
+               <View style={{marginBottom: 2, marginTop: 2}}>
+                    <ScrollView horizontal={true} ref='_compImageListView' showsHorizontalScrollIndicator={false}>
+                        {compList}
+                    </ScrollView>
+                </View>
+                <View>
+                    <ScrollView ref='_compDetailScrollView'>
+                        {defaultComp}
+                    </ScrollView>
+                </View>
+                <View style={{flex: 1}}>
+                    <ScrollView horizontal={true} ref='_compDetailRoundScrollView' showsHorizontalScrollIndicator={false}>
+                        {defaultCompDetailRound}
+                    </ScrollView>
+			    </View>
 			</View>
 		)
 	}
